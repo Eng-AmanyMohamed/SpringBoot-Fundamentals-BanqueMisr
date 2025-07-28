771ab36f313b6b23d8b039da988dc9c412c706b2
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-07-28 17:27:36 +0300

    Delete src/main/java directory

--- src/main/java/com/example/Printer.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-import org.springframework.stereotype.Component;
-  @Component
-public class Printer {
-    public void print(String msg)
-    {
-        System.out.println(msg);
-    }
-}
--- src/main/java/di/Printer.java
+++
@@ -1,7 +0,0 @@
-package main.java.di;
-
-public class Printer {
-        public void print(String msg) {
-            System.out.println(msg);
-        }
-    }
