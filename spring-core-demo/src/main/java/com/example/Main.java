f6b1ee17285e3a2be7aa57565f04b57409b048a9
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-07-28 20:30:19 +0300

    Update Main.java

--- spring-core-demo/src/main/java/com/example/Main.java
+++ spring-core-demo/src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+        //for create spring container
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         UserService userService = context.getBean(UserService.class);
